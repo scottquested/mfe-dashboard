7fd11da7683d70901c97b6bc5e021ae63760e694
Scott Quested <me@scottquested.com>
2022-10-03 11:44:01 +0100

    Update config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
 	mode: "production",
 	output: {
 		filename: "[name].[contenthash].js",
-		publicPath: "/",
+		publicPath: "/dashboard",
 	},
 	plugins: [
 		new ModuleFederationPlugin({
