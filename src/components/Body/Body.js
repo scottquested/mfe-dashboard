4889ef06f8522d22877a60962655eb6bde762791
Scott Quested <me@scottquested.com>
2022-10-03 10:14:16 +0100

    initial commit

---
+++ src/components/Body/Body.js
@@ -0,0 +1,7 @@
+import "./Body.scss";
+
+const Body = () => {
+	return <div className="mfe-dashboard-body">Dashboard MFE body</div>;
+};
+
+export default Body;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import "./Header.scss";
+
+const Header = () => {
+	return <div className="mfe-dashboard-header">Dashboard MFE header</div>;
+};
+
+export default Header;
